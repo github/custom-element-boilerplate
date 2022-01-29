325a793b7dbaf158985a8a424dd02cb17156ed4b
Ned Schwartz <ned@theinterned.net>
2022-01-29 17:49:07 +0000

    configure karma to use chromium

--- karma.config.cjs
+++ karma.config.cjs
@@ -1,4 +1,6 @@
-module.exports = function(config) {
+process.env.CHROME_BIN = require('chromium').path
+
+module.exports = function (config) {
   config.set({
     frameworks: ['mocha', 'chai'],
     files: [
@@ -9,7 +11,7 @@ module.exports = function(config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    browsers: ['ChromeHeadless', 'FirefoxHeadless'],
+    browsers: ['ChromeHeadless'],
     autoWatch: false,
     singleRun: true,
     concurrency: Infinity
